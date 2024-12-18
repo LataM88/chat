6f82ac5df7beb2f1430b73b28de4d7456f066739
Michał Łata <latamichal88@gmail.com>
2024-12-18 19:27:33 +0100

    login/register section with css

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/register';
+import Login from './components/Login/login';
 
 function App() {
   return (
-    <Register />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
